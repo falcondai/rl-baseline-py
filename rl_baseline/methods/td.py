408f8b1f5a48ed6284e1f1ebb118497e93a737ee
Falcon Dai <me@falcondai.com>
2017-11-17 19:40:41 -0600

    add TD-Sarsa

--- rl_baseline/methods/td.py
+++ rl_baseline/methods/td.py
@@ -126,3 +126,115 @@ class QTrainer(Trainer):
             # Stop
             if max_ticks < self.tick:
                 break
+
+
+@method_registry.register('td-sarsa')
+class SarsaTrainer(Trainer):
+    @classmethod
+    def scaffold(kls, env_id, model_id, simulator_id=None):
+        assert simulator_id == None, 'TD(lambda) does not use simulator.'
+
+        from rl_baseline.registration import env_registry, model_registry
+        kwargs = dict(
+            env = env_registry[env_id].make(),
+            model = model_registry[model_id],
+        )
+        return kwargs
+
+    def __init__(self, env, model, td_lambda, gamma, exploration_type, initial_exploration, terminal_exploration, exploration_start, exploration_length, initial_step_size, step_size_schedule, eligibility_type, train_summary_writer=None, eval_summary_writer=None, eval_env=None, n_eval_episodes=0, gpu_id=None):
+        # TODO relax this to non-discrete finite spaces
+        assert isinstance(env.observation_space, spaces.Discrete), 'env.observation_space must be discrete.'
+        assert isinstance(env.action_space, spaces.Discrete), 'env.action_space must be discrete.'
+        assert exploration_type in ['softmax', 'epsilon'], 'Only supports `softmax` and `epsilon`-greedy exploration strategies.'
+        assert eligibility_type in ['accumulating', 'replacing'], 'Only supports `accumulating` and `replacing` eligibility traces.'
+
+        super().__init__(env=env, model=model, eval_env=eval_env, n_eval_episodes=n_eval_episodes, gpu_id=gpu_id)
+
+        self.td_lambda = td_lambda
+        self.n_states = self.env.observation_space.n
+        self.n_actions = self.env.action_space.n
+        self.exploration_type = exploration_type
+        self.gamma = gamma
+        self.eligibility_type = eligibility_type
+        self.initial_exploration = initial_exploration
+        self.terminal_exploration = terminal_exploration
+        self.exploration_start = exploration_start
+        self.exploration_length = exploration_length
+        self.initial_step_size = initial_step_size
+        self.step_size_schedule = step_size_schedule
+        # self.criterion = nn.MSELoss(size_average=False)
+
+    def sample_exploratory_action(self, q, exploration):
+        if self.exploration_type == 'softmax':
+            # Softmax exploration
+            temperature = exploration
+            assert temperature > 0, 'Softmax temperature be greater than 0.'
+            ac = torch.multinomial((q / temperature).exp(), 1)
+            t_ac = ac.data[0, 0]
+        else:
+            # In this case, self.exploration_type == 'epsilon'
+            # Epsilon-greedy exploration
+            epsilon = exploration
+            assert epsilon <= 1, 'Epsilon must be no greater than 1.'
+            if np.random.rand() < epsilon:
+                t_ac = np.random.randint(self.n_actions)
+            else:
+                # Greedy action
+                # max_q, ac = q.max(1)
+                # t_ac = ac.data[0]
+                t_ac = np.argmax(q)
+        return t_ac
+
+    def sample_action_q(self, ob):
+        # v_obs = self.model.preprocess_obs([ob])
+        # q = self.model.q(v_obs)
+        q = self.model[ob]
+        exploration = linear_schedule(self.initial_exploration, self.terminal_exploration, self.exploration_start, self.exploration_length, self.tick)
+        return self.sample_exploratory_action(q, exploration), q
+
+    def train_for(self, max_ticks):
+        # Set eligibility traces to 0
+        e = np.zeros((self.n_states, self.n_actions))
+        ob = self.env.reset()
+        ac, q = self.sample_action_q(ob)
+        while self.tick < max_ticks:
+            next_ob, r, done, extra = self.env.step(ac)
+            self.update_stats(r, done)
+            next_ac, next_q = self.sample_action_q(next_ob)
+
+            # Compute TD-error = (r + gamma * Q(s', a')) - Q(s, a)
+            # v_obs = self.model.preprocess_obs([next_ob])
+            # next_va = self.model.va(v_obs) if not done else 0
+            # next_va = self.model[ob].max() if not done else 0
+            # q_ac = extra['td.q'][ac]
+            next_va = next_q[next_ac] if not done else 0
+            q_ac = q[ac]
+            delta = (r + self.gamma * next_va) - q_ac
+            # Update the eligibility trace of (s, a)
+            if self.eligibility_type == 'accumulating':
+                e[ob, ac] += 1
+            elif self.eligibility_type == 'replacing':
+                e[ob, ac] = 1
+            if delta != 0:
+                # Update action values
+                alpha = self.step_size_schedule(self.tick)
+                # TODO make this more efficient
+                for s in xrange(self.n_states):
+                    for a in xrange(self.n_actions):
+                        self.model[s, a] += alpha * delta * e[s, a]
+                        e[s, a] *= self.td_lambda * self.gamma
+
+            self.report_step(self.n_eval_episodes)
+            self.step += 1
+
+            if done:
+                e = np.zeros((self.n_states, self.n_actions))
+                ob = self.env.reset()
+                ac, q = self.sample_action_q(ob)
+            else:
+                ac, q = next_ac, next_q
+                ob = next_ob
+
+            # Stop
+            if max_ticks < self.tick:
+                break
--- rl_baseline/tests/test_td.py
+++ rl_baseline/tests/test_td.py
@@ -1,9 +1,16 @@
 import numpy as np
-from rl_baseline.methods.td import QTrainer
+from rl_baseline.methods.td import QTrainer, SarsaTrainer
 
-def test_td():
+def test_td_q():
     from gym.envs.toy_text import FrozenLakeEnv
     env = FrozenLakeEnv(is_slippery=False, map_name='4x4')
     model = np.ones((env.observation_space.n, env.action_space.n))
     tra = QTrainer(env=env, model=model, td_lambda=0.6, gamma=0.99, eligibility_type='accumulating', exploration_type='epsilon', initial_exploration=0.1, terminal_exploration=0.1, exploration_start=0, exploration_length=1000, initial_step_size=1, step_size_schedule=lambda tick : 1 / tick ** 0.5)
+    tra.train_for(5000)
+
+def test_td_sarsa():
+    from gym.envs.toy_text import FrozenLakeEnv
+    env = FrozenLakeEnv(is_slippery=False, map_name='4x4')
+    model = np.ones((env.observation_space.n, env.action_space.n))
+    tra = SarsaTrainer(env=env, model=model, td_lambda=0.6, gamma=0.99, eligibility_type='accumulating', exploration_type='epsilon', initial_exploration=0.1, terminal_exploration=0.1, exploration_start=0, exploration_length=1000, initial_step_size=1, step_size_schedule=lambda tick : 1 / tick ** 0.5)
     tra.train_for(5000)
